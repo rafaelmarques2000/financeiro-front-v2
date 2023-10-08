820dd937c2e302e91387c78562e3fafed3fafb6f
Rafael <rafaelmarquespaixao@outlook.com>
2023-10-07 21:48:55 -0300

    implementa grafico de balanco anual

--- src/services/api/dashboardService.js
+++ src/services/api/dashboardService.js
@@ -5,22 +5,38 @@ import Chart from "chart.js/auto";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import {formatMoneyBRL} from "@/helper/moneyHelper";
 
+
+function destroyChart() {
+    let oldChart = Chart.getChart('expenseChart');
+
+    if (oldChart !== undefined) {
+        oldChart.destroy()
+    }
+}
+
 const getInvoiceReport = async (data) => {
     let userId = Store.getters.userData.user_id
+
     try {
-        let url = `/users/${userId}/dashboard/invoice-report?competence_year=2023`
+        let url = `/users/${userId}/dashboard/invoice-report?competence_year=${data.filters.year}`
         let request = await httpService.get(url)
         data.dashboard.expense = request.data;
 
+        if(!data.dashboard.expense.length){
+            destroyChart()
+            return
+        }
+        destroyChart();
+
         let chart = new Chart(
-            document.getElementById('chart'),
+            document.getElementById('expenseChart'),
             {
                 scales: {
                     y: {
                         beginAtZero: true
                     }
                 },
-                responsive: true,
+                responsive: false,
                 maintainAspectRatio: false,
                 type: 'bar',
                 plugins:[ChartDataLabels],
@@ -42,18 +58,30 @@ const getInvoiceReport = async (data) => {
                         }
                     }
                 },
+                layout:{
+                  padding:{
+                      top: 200
+                  }
+                },
                 data: {
                     labels: data.dashboard.expense.map(row => row.month),
                     datasets: [
                         {
                             label: "Gastos Mensais",
-                            data: data.dashboard.expense.map(row => row.amount)
+                            data: data.dashboard.expense.map(row => row.amount),
+                            backgroundColor: [ // Cores para cada barra
+                                'rgba(255, 99, 132, 0.5)', // Cor para a Barra 1
+                                'rgba(54, 162, 235, 0.5)', // Cor para a Barra 2
+                                'rgba(255, 206, 86, 0.5)' // Cor para a Barra 3
+                            ],
                         }
                     ]
                 }
             }
         );
+
     }catch (e) {
+        console.log(e)
         alertError("Atenção","Falha ao obter dados do dashboard");
     }
 }
--- src/views/dashboard/dashboardview.vue
+++ src/views/dashboard/dashboardview.vue
@@ -1,7 +1,36 @@
 <template>
     <div class="container-fluid">
         <page-title page-subtitle="Gerencie suas finanças" page-title="Dashboard" icon="fa-solid fa-chart-line"></page-title>
-        <canvas id="chart"></canvas>
+      <ul class="nav nav-tabs" id="myTabs" role="tablist">
+        <li class="nav-item" role="presentation">
+          <a class="nav-link active" id="tab1-tab" data-bs-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="true">Balanço anual</a>
+        </li>
+        <li class="nav-item" role="presentation">
+          <a class="nav-link" id="tab2-tab" data-bs-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Balanço mensal</a>
+        </li>
+      </ul>
+
+      <div class="tab-content" id="myTabsContent">
+
+        <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+
+            <div class="container-fluid dash-expense-filter d-flex align-items-md-end justify-content-end">
+                <select @change="updateBarChart" v-model="data.filters.year" class="form-select">
+                    <option v-for="item in data.years" :value="item">{{item}}</option>
+                </select>
+            </div>
+
+          <no-content v-if="!data.dashboard.expense.length" message="Não há dados para mostrar"></no-content>
+
+            <div class="container-fluid expense-chart-content">
+               <canvas id="expenseChart" height="100"></canvas>
+            </div>
+        </div>
+
+        <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+          Contenido de la Tab 2
+        </div>
+      </div>
     </div>
 </template>
 
@@ -9,30 +38,55 @@
    import PageTitle from "@/components/page_title/pagetile.vue";
    import {onMounted, reactive} from "vue";
    import {getInvoiceReport} from "@/services/api/dashboardService";
+   import NoContent from "@/components/nocontent/NoContent.vue";
    export default {
        name : "dashboard",
-     components: {PageTitle},
+     components: {NoContent, PageTitle},
        setup(){
 
          const data = reactive({
+              loading:{
+                show: false
+              },
+              years: [],
+              filters: {
+                year: ""
+              },
               dashboard:{
-                  expense: null
+                  expense: []
               }
          })
 
-          onMounted(() => {
+         const generateSelectYears = () => {
+             let currentYear = new Date().getFullYear()
+             const yearsToShow = 5;
 
+             // Loop para gerar a lista de anos
+             for (let i = currentYear; i <= currentYear + yearsToShow; i++) {
+                  data.years.push(i)
+             }
+             data.filters.year = currentYear
+         }
+
+         const updateBarChart = () => {
+             getInvoiceReport(data)
+         }
+
+
+          onMounted(() => {
+            generateSelectYears()
             getInvoiceReport(data)
 
           })
 
          return {
-           data
+           data,
+           updateBarChart
          }
        }
    }
 </script>
 
 <style scoped lang="scss">
-
+ @import "dashboardstyle";
 </style>
