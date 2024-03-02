c044dcb82f6aa617a06d0d62379ee3671bf08155
rafael <rafaelmarquespaixao@outlook.com>
2024-03-02 17:47:24 -0300

    ajusta dash para o grafana

--- src/views/module_select/moduleselectview.vue
+++ src/views/module_select/moduleselectview.vue
@@ -4,7 +4,7 @@
           <page-title page-subtitle="Escolha um modulo para continuar" page-title="Seja bem-vindo, Admin"></page-title>
           <div class="row row-cols-2">
               <div class="col-md-2 col-margin" v-for="item in moduleList">
-                   <div class="card module-card" @click="openModule(item.url, item.standalone)">
+                   <div class="card module-card" @click="openModule(item.url, item.standalone , item.external)">
                         <div class="card-body flex-column d-flex align-items-center justify-content-center">
                           <font-awesome-icon class="module-icon" :icon="item.icon" />
                           <span class="module-name">{{item.title}}</span>
@@ -29,55 +29,67 @@
                {
                   title: "Dashboard",
                   icon: "fa-solid fa-chart-line",
-                  url: "dashboard_module",
+                  url: "https://grafana.globalapps.xyz/d/fc2129c0-7323-4414-90c8-ed4a09dd97a2/gastos-mensais-graninha-4-0?orgId=1",
+                  external: true,
                   standalone: true
                },
                {
                  title: "Contas",
                  icon: "fa-solid fa-receipt",
                  url:"contas",
+                 external: false,
                  standalone: false
                },
                {
                  title: "Cartões",
                  icon: "fa-solid fa-credit-card",
                  url:"cartoes",
+                 external: false,
                  standalone: false
                },
                {
                  title: "Caixa",
                  icon: "fa-solid fa-money-bill",
                  url: "caixa",
+                 external: false,
                  standalone: false
                },
                {
                  title: "Cofrinhos",
                  icon: "fa-solid fa-piggy-bank",
                  url: "goals_module",
+                 external: false,
                  standalone: true
                },
              {
                title: "Lista de desejos",
                icon: "fa-solid fa-heart",
                url: "wishlist",
+               external: false,
                standalone: true
              },
              {
                title: "Historico Transações",
                icon: "fa-solid fa-clock",
                url: "transaction_history",
-               standalone: true
+               standalone: true,
+               external: false,
              }
            ]
 
          let router = useRouter()
 
-         const openModule = (url, standalone) => {
+         const openModule = (url, standalone, external) => {
              if(standalone){
                 router.push({
                     name: url
                 })
                 return
+             }
+
+             if(external) {
+               window.open(url, '_blank');
+               return;
              }
 
             router.push(
