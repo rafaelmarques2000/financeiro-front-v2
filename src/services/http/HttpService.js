81ff9988c0fc1eaf31d570aa92beebec05f02e6a
rafael <rafaelmarquespaixao@outlook.com>
2024-12-01 17:52:32 -0300

    commit

--- src/services/http/HttpService.js
+++ src/services/http/HttpService.js
@@ -6,7 +6,7 @@ import store from "../../store/index"
 // }
 
 let axiosOption = {
-    baseURL:"http://192.168.254.202:8095/api/v1",
+    baseURL:"http://192.168.254.211:8092/api/v1",
 }
 
 let client = axios.create(axiosOption)
